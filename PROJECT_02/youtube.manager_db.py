309dbaaddc1981a5488aa4cf55418b61b5205727
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-04-14 12:01:40 +0530

    Updated project files

--- PROJECT_02/youtube.manager_db.py
+++ PROJECT_02/youtube.manager_db.py
@@ -11,6 +11,23 @@ cursor.execute('''
     )
 ''')
 
+def list_video():
+    cursor.execute("SELECT * FROM videos")  # tuple
+    for row in cursor.fetchall():
+        print(row)
+
+def add_video(name,time):
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?,?)", (name,time))
+    conn.commit()
+
+def update_video(video_id,new_name,new_time):
+    cursor.execute("UPDATE videos SET name=?, time=? WHERE id=?",(new_name,new_time,video_id))
+    conn.commit()
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos where id=?", (video_id,))   #sinle parameter ke baad always comma
+    conn.commit()
+
 def main():
     while True:
         print("\n Youtube manager app with DB")
@@ -20,6 +37,27 @@ def main():
         print("4. Delete videos")
         print("5. exit app")
         choice=input("enter your choice: ")
+
+        if choice=='1':
+            list_video()
+        elif choice =='2':
+            name=input("enter the video name: ")
+            time=input("enter the video time: ")
+            add_video(name,time)
+        elif choice=='3':
+            video_id =input("enter the video id to update:")
+            name=input("enter the video name: ")
+            time=input("enter the video time: ")
+            update_video(video_id,name,time)
+        elif choice=='4':
+            video_id =input("enter the video id to delete:")
+            delete_video(video_id)
+        elif choice=='5':
+            break
+        else:
+            print("invalid choice")
+
+    conn.close()
 
 if __name__=="__main__":
     main()
